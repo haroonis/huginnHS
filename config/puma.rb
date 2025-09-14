aeef4a76da7d8755736285d04936be493736edda
haroonis <haroon_shafiq@hotmail.com>
2025-09-14 20:50:58 +0100

    Create puma.rb
    Going by Perplexity advice to create this file

---
+++ config/puma.rb
@@ -0,0 +1,15 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
